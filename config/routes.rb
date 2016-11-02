131a0e9fb1c4df1f190ffb23fca0f0becb532c1b
Afke Holgersen <Afke@d207-023-183-075.wireless.sfu.ca>
2016-11-02 12:49:25 -0700

    Integrating UI and Ruby, added images

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :users
+
+  root to: 'welcome#index' #main home webpage
+  get "welcome/index"
+  get "users/new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
