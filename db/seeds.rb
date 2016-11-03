50166d29408d6f30e12affefd9fe064db1fb795d
Afke Holgersen <aholgers@sfu.ca>
2016-11-03 14:52:12 -0700

    Added links to homepage, changed them to buttons

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Dietlabel.create([  
+Dietlabel.create([
 
 {name: 'Balanced', apiparameter: 'balanced', description: 'Protein/Fat/Carb values in 15/35/50 ratio'}, 
 {name: 'High-Fiber', apiparameter: 'high-fiber', description: 'More than 5g fiber per serving'},
