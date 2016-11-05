2519f6c58d61da4f1a555648fda33573ca70f0b6
khashiyousefi <kashy@sfu.ca>
2016-11-05 13:46:34 -0700

    created total tables

---
+++ db/migrate/20161105093710_create_total_nutrient_nodes.rb
@@ -0,0 +1,14 @@
+class CreateTotalNutrientNodes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :total_nutrient_nodes do |t|
+      t.string :label
+      t.string :quantity
+      t.string :unit
+      t.string :node_label
+      t.integer :recipe_id
+
+
+      t.timestamps
+    end
+  end
+end
