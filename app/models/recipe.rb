3b61f99d5700ecf24167a7331d40ad966661b226
khashiyousefi <kashy@sfu.ca>
2016-11-05 13:17:20 -0700

    modify recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ApplicationRecord
 	has_and_belongs_to_many :savedrecipe
+	belongs_to :user
+  has_many :ingredients, dependent: :destroy
+  has_many :ingredient_lines, dependent: :destroy
+  has_many :total_daily_nodes, dependent: :destroy
+  has_many :total_nutrient_nodes, dependent: :destroy
 end
