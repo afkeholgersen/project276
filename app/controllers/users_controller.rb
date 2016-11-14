3b218e2846d8c842b0e32d357c0c1eed9440b744
Afke Holgersen <afkeh@Afkes-iMac.local>
2016-11-13 16:20:43 -0800

    added and tested Email verification using a gem. #23

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :home, :save_recipe, :my_recipes]
-
+  
   # GET /users
   # GET /users.json
   def index
