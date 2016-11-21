1e350a76898724f48701c21ffea31866a87528aa
Afke Holgersen <aholgers@sfu.ca>
2016-11-21 13:48:12 -0800

    comment board in progress

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,6 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     @user = User.find_by(id: session[:user_id])
-
-    
+    @comment = Comment.new(comment_params)
   end
 end
