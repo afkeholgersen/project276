58474585c8af53c1a82bc65e98d0919bf8688919
Afke Holgersen <aholgers@sfu.ca>
2016-11-21 16:16:07 -0800

    comment board still in progress..

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,19 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
-    @user = User.find_by(id: session[:user_id])
-    @comment = Comment.new(comment_params)
+        #@comment = Comment.new(comment_params)
   end
+
+  def create
+    #u = User.find_by(id: session[:user_id])
+    @recipe = Recipe.find(params[:commit].id)
+    @comment = Comment.new(:comment_text => params[:comment_text], :vote => params[:vote])
+    current_user.comments.push(@comment)
+    @recipe.comments.push(@comment)
+    @comment.user = current_user
+    @comment.recipe = @recipe
+    @comment.save
+  end
+
+
 end
