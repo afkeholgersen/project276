05e7fcee90a37f34ada1ca1fcee4b9b698eaa497
mehrshad9821 <mehrshad9821@Hotmail.com>
2016-12-03 12:40:14 -0800

    Add files via upload

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,19 +3,19 @@ require 'test_helper'
 class SessionsControllerTest < ActionDispatch::IntegrationTest
 
 	test "create successful session" do
-  	@user = users(:one)
-    @user.email="test@test.com"
-    @user.password="test"
-    @user.username="test"
+
 		#create the user first
     assert_difference('User.count') do
+    	@user = users(:one)
+      @user.email="test@test.com"
+      @user.password="test"
+      @user.username="test"
       post users_url, params: { user: {email: @user.email, password: @user.password, username: @user.username }, preference: { healthlabel_id: @healthlabel, dietlabel_id: @dietlabel } }
     end
 
     #login with the user
 		post "/sessions", params: { username: "test", password: "test"}
-		assert_response :redirect
-
+		assert_redirected_to users_url
 	end
 
 
