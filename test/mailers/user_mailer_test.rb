bf0fa6b0735d21c9d3da2e8f7846e222d88a7e3c
Afke Holgersen <afkeh@Afkes-iMac.local>
2016-12-05 11:24:46 -0800

    add footer and fix advanced search UI mistake i made

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,12 +2,10 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "password_reset" do
-    user=User.new
-    user.email="to@example.com"
-    user.username="example"
-    user.password="password"
-    mail = UserMailer.password_reset(user[:id])
-    assert_equal "Password Reset", mail.subject
+    user = users(:one)
+    get :id, users(:one)
+    mail = UserMailer.password_reset(user)
+    assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
   end
 
